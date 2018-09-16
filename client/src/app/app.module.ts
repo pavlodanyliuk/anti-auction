905b45fbc914baffac7cbbdd057d6459fde5401d
kamai <pavlodanyliuk@gmail.com>
2018-09-16 21:04:15 +0300

    Creating a simple landing page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { HelloComponent } from './hello/hello.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
